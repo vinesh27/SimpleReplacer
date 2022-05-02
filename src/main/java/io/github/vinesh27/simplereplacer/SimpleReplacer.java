128065bb4e4e709da3c6d41ea4bcf0050acb98f9
vinesh27 <vinitrajpurohit00@gmail.com>
2022-05-02 15:30:54 +0530

    Project Creation

---
+++ src/main/java/io/github/vinesh27/simplereplacer/SimpleReplacer.java
@@ -0,0 +1,17 @@
+package io.github.vinesh27.simplereplacer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimpleReplacer extends JavaPlugin {
+    
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        
+    }
+    
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
