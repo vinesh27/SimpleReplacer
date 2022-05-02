e78dd436072972e761fcc530f2edf9c922449b95
vinesh27 <vinitrajpurohit00@gmail.com>
2022-05-02 17:25:01 +0530

    Fixed Reloading

--- src/main/java/io/github/vinesh27/simplereplacer/ReloadCommand.java
+++ src/main/java/io/github/vinesh27/simplereplacer/ReloadCommand.java
@@ -4,6 +4,8 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+import java.util.HashMap;
+
 public class ReloadCommand implements CommandExecutor {
     private final SimpleReplacer plugin;
     public ReloadCommand(SimpleReplacer plugin) {
@@ -13,6 +15,12 @@ public class ReloadCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         plugin.reloadConfig();
+        HashMap<String, String> map = new HashMap<>();
+        plugin.getConfig().getConfigurationSection("replacements")
+            .getKeys(false)
+            .forEach(key ->
+                map.put(key, plugin.getConfig().getString("replacements." + key)));
+        SimpleReplacer.replacements = map;
         sender.sendMessage("Reloaded config");
         return true;
     }
